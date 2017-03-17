3b0aa2ebf8ead795a5e0fcf24623721076a03ad8
Hamza Haiken <hamza.haiken@ordina.nl>
2017-03-17 13:57:34 +0100

    Add solution and tests

--- ScalaAssessment1/src/main/scala/Exercise1.scala
+++ ScalaAssessment1/src/main/scala/Exercise1.scala
@@ -1,5 +1,14 @@
 object Exercise1 {
-
-  def scrapeBumps = ???
-
+  def scrapeBumps[T](input: Seq[T])(implicit num: Numeric[T]) = {
+    input match {
+      case Nil => Nil
+      case _ =>
+        input.tail.foldLeft(Seq[T](input.head)) { (accu, n) =>
+          if (num.gt(n, accu.head))
+            accu.head +: accu
+          else
+            n +: accu
+        }.reverse
+    }
+  }
 }
--- ScalaAssessment1/src/test/scala/Exercise1Test.scala
+++ ScalaAssessment1/src/test/scala/Exercise1Test.scala
@@ -1,15 +1,45 @@
-import org.scalacheck.Gen
+import org.scalacheck.Prop.{AnyOperators, forAll}
 import org.scalatest.PropSpec
 import org.scalatest.prop.Checkers
 
+import Exercise1.scrapeBumps
+
 class Exercise1Test extends PropSpec with Checkers {
 
-  property("#1: scraping bumps off a list results in decending lists") {
-//    check(
-//
-//    )
-
+  property("#1: scraping bumps off a list results in descending lists") {
+    check(
+      forAll { numbers: Seq[Int] =>
+        val result = scrapeBumps(numbers)
+        result.sortWith(_ > _) ?= result
+      }
+    )
   }
 
+  property("#2: length of output list doesn't change") {
+    check(
+      forAll { numbers: Seq[Int] =>
+        val result = scrapeBumps(numbers)
+        result.length ?= numbers.length
+      }
+    )
+  }
+
+  property("#3: the first element of the output should not be altered") {
+    check(
+      forAll { numbers: Seq[Int] =>
+        val result = scrapeBumps(numbers)
+        result.headOption ?= numbers.headOption
+      }
+    )
+  }
+
+  property("#4: the minimum number should be the same in output and input") {
+    check(
+      forAll { numbers: Seq[Int] =>
+        val result = scrapeBumps(numbers)
+        result.sortWith(_ < _).headOption ?= numbers.sortWith(_ < _).headOption
+      }
+    )
+  }
 
 }
